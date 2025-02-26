d3714a8c478685d4de75e76b8010c49a23882ba6
theneelshah <theneelshah@gmail.com>
2025-02-25 18:52:39 -0800

    fix: fix commitizen configs
    Add the test prefix
     Fix the new line delimiter
    
    Tests: -

--- .cz-config.js
+++ .cz-config.js
@@ -13,12 +13,13 @@ export const allowBreakingChanges = false;
 export const skipQuestions = ["breaking"];
 export const subjectLimit = 100;
 export const allowTicketNumber = false;
-export const footerPrefix = "";
+export const footerPrefix = "Tests:";
 export const messages = {
   type: "Select the type of change that you're committing:",
   scope: "Specify the scope of this change (optional):",
   subject: "Write a short, imperative mood description of the change:\n",
-  body: "Provide a longer description of the change (optional). Use | to break new lines:\n",
-  footer: "List any test details (optional). Use | for new lines:\n",
+  body: "Provide a longer description of the change (optional). Use '\\n' for new lines:\n",
+  footer:
+    "List any test details or references (optional). Use '\\n' for new lines:\n",
   confirmCommit: "Are you sure you want to proceed with the commit?",
 };
