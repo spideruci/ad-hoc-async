b31892774c813f3b85e0cc5fd135e323a6d2b26a
hank <hank0982@gmail.com>
2025-02-27 15:44:54 -0800

    feat: update terminal view

--- src/terminal-webview/TerminalApp.tsx
+++ src/terminal-webview/TerminalApp.tsx
@@ -9,7 +9,18 @@ import { ThemeProvider, createTheme } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import IconButton from "@mui/material/IconButton";
 import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
-import type { ConsoleLog, ToEditorMessage, ToVSCodeMessage, VSCodeState } from "../types/message";
+import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
+import Collapse from "@mui/material/Collapse";
+import ExpandLess from "@mui/icons-material/ExpandLess";
+import ExpandMore from "@mui/icons-material/ExpandMore";
+import { Card, CardActions, CardContent, Typography } from "@mui/material";
+import ReactJson from "react-json-view";
+import type {
+  ConsoleLog,
+  ToEditorMessage,
+  ToVSCodeMessage,
+  VSCodeState,
+} from "../types/message";
 
 interface FunctionAndCallContent {
   logs: Array<ConsoleLog | null>;
@@ -20,17 +31,26 @@ interface FunctionAndCallContent {
   listName: string;
 }
 interface FunctionAndCallMap {
-  [functionNameOrKey: string]: FunctionAndCallContent
+  [functionNameOrKey: string]: FunctionAndCallContent;
 }
 const darkTheme = createTheme({
   palette: { mode: "dark" },
 });
 
-function customSortForFlatFunctionAndCallMap(a: [string, FunctionAndCallContent], b: [string, FunctionAndCallContent]) {
-  const functionNameComparison = a[1].functionName!.localeCompare(b[1].functionName!);
+function customSortForFlatFunctionAndCallMap(
+  a: [string, FunctionAndCallContent],
+  b: [string, FunctionAndCallContent]
+) {
+  const functionNameComparison = a[1].functionName!.localeCompare(
+    b[1].functionName!
+  );
   if (functionNameComparison === 0) {
-    if (a[1].functionKey && !b[1].functionKey) {return 1;}
-    if (b[1].functionKey && !a[1].functionKey) {return -1;}
+    if (a[1].functionKey && !b[1].functionKey) {
+      return 1;
+    }
+    if (b[1].functionKey && !a[1].functionKey) {
+      return -1;
+    }
     return a[1].firstLogTimestamp - b[1].firstLogTimestamp;
   }
   return functionNameComparison;
@@ -40,17 +60,22 @@ const vscode = acquireVsCodeApi<VSCodeState, ToVSCodeMessage>();
 const TerminalApp = (): JSX.Element => {
   const [logs, setLogs] = useState<ConsoleLog[]>([]);
   const [autoScroll, setAutoScroll] = useState(true);
-  const [splitBySpecificFunctionSet, setSplitBySpecficFunctionSet] = useState(new Set<string>());
-  const [splitBySpecificFunctionCallSet, setSplitBySpecificFunctionCallSet] = useState(new Set<string>());
+  const [splitBySpecificFunctionSet, setSplitBySpecficFunctionSet] = useState(
+    new Set<string>()
+  );
+  const [rowOpenSet, setRowOpenSet] = useState(new Set<number>());
+  const [splitBySpecificFunctionCallSet, setSplitBySpecificFunctionCallSet] =
+    useState(new Set<string>());
   const listRef = useRef<HTMLDivElement>(null);
-
 
   const handleLogHover = useCallback((logId: string): void => {
     vscode.postMessage({ command: "logHover", logId });
   }, []);
 
   const handleScroll = useCallback((): void => {
-    if (!listRef.current) { return; }
+    if (!listRef.current) {
+      return;
+    }
     const { scrollTop, scrollHeight, clientHeight } = listRef.current;
     setAutoScroll(scrollTop + clientHeight >= scrollHeight - 5);
   }, [listRef]);
@@ -76,80 +101,99 @@ const TerminalApp = (): JSX.Element => {
     return (): void => window.removeEventListener("message", handleMessage);
   }, [handleMessage]);
 
+  const handleSpecificFunctionCallSplits = useCallback(
+    (functionName: string, functionKey: string) => {
+      setSplitBySpecificFunctionCallSet((prev) => {
+        const key = `${functionName}-${functionKey}`;
+        const newSet = new Set([...prev, key]);
+        if (prev.has(key)) {
+          newSet.delete(key);
+        }
+        return newSet;
+      });
+    },
+    [setSplitBySpecificFunctionCallSet]
+  );
 
-  const handleSpecificFunctionCallSplits = useCallback((functionName: string, functionKey: string) => {
-    setSplitBySpecificFunctionCallSet((prev) => {
-      const key = `${functionName}-${functionKey}`;
-      const newSet = new Set([...prev, key]);
-      if (prev.has(key)) { newSet.delete(key); }
-      return newSet;
-    });
-  }, [setSplitBySpecificFunctionCallSet]);
-
-  const handleSpecificFunctionSplits = useCallback((functionName: string) => {
-    setSplitBySpecficFunctionSet((prev) => {
-      const newSet = new Set([...prev, functionName]);
-      if (prev.has(functionName)) { newSet.delete(functionName); }
-      return newSet;
-    });
-  }, [setSplitBySpecficFunctionSet]);
-
+  const handleSpecificFunctionSplits = useCallback(
+    (functionName: string) => {
+      setSplitBySpecficFunctionSet((prev) => {
+        const newSet = new Set([...prev, functionName]);
+        if (prev.has(functionName)) {
+          newSet.delete(functionName);
+        }
+        return newSet;
+      });
+    },
+    [setSplitBySpecficFunctionSet]
+  );
+  const handleListItemExpandClick = useCallback(
+    (rowIndex: number) => {
+      setRowOpenSet((prev) => {
+        const newSet = new Set([...prev, rowIndex]);
+        if (prev.has(rowIndex)) {
+          newSet.delete(rowIndex);
+        }
+        return newSet;
+      });
+    },
+    [setRowOpenSet]
+  );
   const functionLogMap = useMemo(() => {
     const filteredLogs = logs.filter((log) => log.type === "console.log");
     const functionLogMap: FunctionAndCallMap = {};
     let currentLength = 0;
-    filteredLogs.sort((a, b) => a.timestamp - b.timestamp).forEach((v) => {
-      const key = `${v.functionName}-${v.functionKey}`;
+    filteredLogs
+      .sort((a, b) => a.timestamp - b.timestamp)
+      .forEach((v) => {
+        const key = `${v.functionName}-${v.functionKey}`;
 
-      if (splitBySpecificFunctionCallSet.has(key)) {
-        if (!functionLogMap[key]) {
-          functionLogMap[key] = {
-            listName: `${v.functionName}-${v.functionKey}`,
-            logs: new Array(currentLength).fill(null),
-            type: "call",
-            functionKey: v.functionKey,
-            functionName: v.functionName,
-            firstLogTimestamp: v.timestamp
-          };
+        if (splitBySpecificFunctionCallSet.has(key)) {
+          if (!functionLogMap[key]) {
+            functionLogMap[key] = {
+              listName: `${v.functionName}-${v.functionKey}`,
+              logs: new Array(currentLength).fill(null),
+              type: "call",
+              functionKey: v.functionKey,
+              functionName: v.functionName,
+              firstLogTimestamp: v.timestamp,
+            };
+          }
+          Object.keys(functionLogMap).map((k) => {
+            functionLogMap[k].logs.push(k === key ? v : null);
+          });
+        } else if (splitBySpecificFunctionSet.has(v.functionName)) {
+          if (!functionLogMap[v.functionName]) {
+            functionLogMap[v.functionName] = {
+              listName: `${v.functionName}`,
+              logs: new Array(currentLength).fill(null),
+              type: "function",
+              functionName: v.functionName,
+              firstLogTimestamp: v.timestamp,
+            };
+          }
+          Object.keys(functionLogMap).map((k) => {
+            functionLogMap[k].logs.push(k === v.functionName ? v : null);
+          });
+        } else {
+          if (!functionLogMap["allLog"]) {
+            functionLogMap["allLog"] = {
+              functionName: "*ALLLOG*",
+              listName: "All Logs",
+              logs: new Array(currentLength).fill(null),
+              type: "allLog",
+              firstLogTimestamp: v.timestamp,
+            };
+          }
+          Object.keys(functionLogMap).map((k) => {
+            functionLogMap[k].logs.push(k === "allLog" ? v : null);
+          });
         }
-        Object.keys(functionLogMap).map(k => {
-          functionLogMap[k].logs.push(k === key ? v : null);
-        });
-      } else if (splitBySpecificFunctionSet.has(v.functionName)) {
-        if (!functionLogMap[v.functionName]) {
-          functionLogMap[v.functionName] = {
-            listName: `${v.functionName}`,
-            logs: new Array(currentLength).fill(null),
-            type: "function",
-            functionName: v.functionName,
-            firstLogTimestamp: v.timestamp
-          };
-        }
-        Object.keys(functionLogMap).map(k => {
-          functionLogMap[k].logs.push(k === v.functionName ? v : null);
-        });
-      } else {
-        if (!functionLogMap["allLog"]) {
-          functionLogMap["allLog"] = {
-            functionName: "*ALLLOG*",
-            listName: "All Logs",
-            logs: new Array(currentLength).fill(null),
-            type: "allLog",
-            firstLogTimestamp: v.timestamp
-          };
-        }
-        Object.keys(functionLogMap).map(k => {
-          functionLogMap[k].logs.push(k === "allLog" ? v : null);
-        });
-      }
-      currentLength++;
-    }, {} as Record<string, (ConsoleLog | null)[]>);
-
-
+        currentLength++;
+      }, {} as Record<string, (ConsoleLog | null)[]>);
 
     return functionLogMap;
   }, [logs, splitBySpecificFunctionCallSet, splitBySpecificFunctionSet]);
-
 
   return (
     <ThemeProvider theme={darkTheme}>
@@ -166,54 +210,132 @@ const TerminalApp = (): JSX.Element => {
               overflowX: "auto",
               display: "flex",
               flexDirection: "row",
-              flexWrap: "nowrap"
+              flexWrap: "nowrap",
             }}
           >
-            {Object
-              .entries(functionLogMap)
+            {Object.entries(functionLogMap)
               .sort(customSortForFlatFunctionAndCallMap)
               .map(([k, functionLogs]) => (
                 <Box key={k} sx={{ minWidth: "400px", flex: "1" }}>
-                  <List dense={true} subheader={<ListSubheader>
-                    {functionLogs.listName}
-                  </ListSubheader>}>
-                    {functionLogs.logs.map((log, index) => (
+                  <List
+                    dense={true}
+                    subheader={
+                      <ListSubheader>
+                        {functionLogs.listName}{" "}
+                        <IconButton
+                          edge="end"
+                          aria-label="delete"
+                          size="small"
+                          onClick={(e) => {
+                            e.stopPropagation();
+                            if (
+                              functionLogs.type !== "allLog" &&
+                              functionLogs.type === "function"
+                            ) {
+                              handleSpecificFunctionSplits(
+                                functionLogs.functionName
+                              );
+                            } else {
+                              handleSpecificFunctionCallSplits(
+                                functionLogs.functionName,
+                                functionLogs.functionKey!
+                              );
+                            }
+                          }}
+                        >
+                          <KeyboardArrowLeftIcon fontSize="inherit" />
+                        </IconButton>
+                      </ListSubheader>
+                    }
+                  >
+                    {functionLogs.logs.map((log, index) =>
                       log === null ? (
-                        <div key={`empty-${functionLogs.functionName}-${index}`}>
-                          <ListItemButton
-                            sx={{ height: "50px", opacity: 0 }}
-                            alignItems="flex-start"
-                          />
+                        <div
+                          key={`empty-${functionLogs.functionName}-${index}`}
+                          style={{
+                            height: rowOpenSet.has(index) ? "80px" : "30px",
+                          }}
+                        >
                           <Divider />
                         </div>
                       ) : (
                         <div key={`log-${functionLogs.functionName}-${index}`}>
                           <ListItemButton
-                            sx={{ height: "50px" }}
-                            alignItems="flex-start"
-                            onMouseEnter={() => handleLogHover(log.logId)}
+                            sx={{ height: "30px" }}
+                            onClick={() => handleListItemExpandClick(index)}
                           >
-                            <ListItemText primary={log.logData.map((data) => JSON.stringify(data)).join(", ")} secondary={`Line: ${log.lineNumber}`}
+                            <ListItemText
+                              primary={String(log.logData[0]).substring(0, 100)}
                             />
-                            <IconButton edge="end" aria-label="delete" size="small" onClick={(e) => {
-                              e.stopPropagation();
-                              if (functionLogs.functionName === "*ALLLOG*") {
-                                handleSpecificFunctionSplits(log.functionName);
-                              } else {
-                                handleSpecificFunctionCallSplits(log.functionName, log.functionKey);
-                              }
-                            }}>
-                              <KeyboardArrowRightIcon fontSize="inherit" />
-                            </IconButton>
+                            {(log.logData.length > 1 ||
+                              String(log.logData[0]).length > 100) ??
+                              (rowOpenSet.has(index) ? (
+                                <ExpandLess />
+                              ) : (
+                                <ExpandMore />
+                              ))}
                           </ListItemButton>
+                          <Collapse
+                            in={rowOpenSet.has(index)}
+                            timeout="auto"
+                            unmountOnExit
+                          >
+                            <Card
+                              sx={{ height: "auto" }}
+                              onMouseEnter={() => handleLogHover(log.logId)}
+                            >
+                              <CardContent>
+                                {log.logData.map((data) => {
+                                  if (typeof data === "object") {
+                                    return <ReactJson
+                                      theme={"monokai"}
+                                      onEdit={false}
+                                      onAdd={false}
+                                      onDelete={false}
+                                      collapseStringsAfterLength={100}
+                                      collapsed={true}
+                                      src={data}
+                                    />;
+                                  } else {
+                                    return (
+                                      <Typography variant="body2">
+                                        {String(data)}
+                                      </Typography>
+                                    );
+                                  }
+                                })}
+                              </CardContent>
+                              <CardActions>
+                                <IconButton
+                                  edge="end"
+                                  aria-label="delete"
+                                  size="small"
+                                  onClick={(e) => {
+                                    e.stopPropagation();
+                                    if (functionLogs.type === "allLog") {
+                                      handleSpecificFunctionSplits(
+                                        log.functionName
+                                      );
+                                    } else {
+                                      handleSpecificFunctionCallSplits(
+                                        log.functionName,
+                                        log.functionKey
+                                      );
+                                    }
+                                  }}
+                                >
+                                  <KeyboardArrowRightIcon fontSize="inherit" />
+                                </IconButton>
+                              </CardActions>
+                            </Card>
+                          </Collapse>
                           <Divider />
                         </div>
                       )
-                    ))}
+                    )}
                   </List>
                 </Box>
-              ))
-            }
+              ))}
           </Box>
         </Box>
       </Box>
