db90fb5fd863ab82adb427fad0eb6403a796bc22
Boyuan Jiang <48397234+bjiang97@users.noreply.github.com>
2025-03-27 22:27:07 -0700

    feat: added the visibility feature

---
+++ src/terminal-webview/components/ClosedEyeIcon.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ClosedEyeIcon: React.FC = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="16"
+    height="16"
+    fill="currentColor"
+    viewBox="0 0 16 16"
+  >
+    <path d="M13.359 11.238a8.93 8.93 0 0 0 2.12-2.238c-1.2-2-3.8-5-7.48-5-1.04 0-2.02.24-2.92.66L3.707 2.293a1 1 0 1 0-1.414 1.414l11 11a1 1 0 0 0 1.414-1.414l-1.348-1.348ZM9.586 10l-3.6-3.6A3 3 0 0 0 9.586 10ZM5.354 6.768l-1.6-1.6C1.694 6.06.52 8.06.52 8.06s2.6 5 7.48 5c1.44 0 2.74-.4 3.88-1.06l-1.6-1.6a5 5 0 0 1-4.926-4.926Z" />
+  </svg>
+);
+
+export default ClosedEyeIcon;
---
+++ src/terminal-webview/components/OpenEyeIcon.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const OpenEyeIcon: React.FC = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="16"
+    height="16"
+    fill="currentColor"
+    viewBox="0 0 16 16"
+  >
+    <path d="M8 3c-4.418 0-8 3.582-8 5s3.582 5 8 5 8-3.582 8-5-3.582-5-8-5zm0 9c-3.314 0-6-2.686-6-4s2.686-4 6-4 6 2.686 6 4-2.686 4-6 4zm0-7a3 3 0 1 0 0 6 3 3 0 0 0 0-6zm0 5a2 2 0 1 1 0-4 2 2 0 0 1 0 4z" />
+  </svg>
+);
+
+export default OpenEyeIcon;
